28024290e265661c19482497f9d375077127265d
WalunjAmol <walunjamol444@gmail.com>
2023-10-24 13:52:54 +0530

    delete function

--- DairyManagementProject/urls.py
+++ DairyManagementProject/urls.py
@@ -19,6 +19,31 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from dairy_owner_management.views import HomeView
+from milk_transaction.models import MilkTransaction
+from django.http import HttpResponse  # Import HttpResponse
+from bonus_app.models import Bonus
+
+
+def deleteData(request):
+    from_date = "2023-08-01"  # Use the 'YYYY-MM-DD' format for dates
+    to_date = "2023-09-30"
+
+    try:
+        delete_data = MilkTransaction.objects.filter(date__range=(from_date, to_date))
+        bonus_date = Bonus.objects.filter(bonus_date__range=(from_date, to_date))
+        print('bonus_datebonus_datebonus_date',bonus_date)
+        for data in delete_data:
+            data.delete()  # Delete each data object
+            print(f'Deleted data with date: {data.date}')
+
+        for data in bonus_date:
+            data.delete()  # Delete each data object
+            print(f'Deleted data with date: {data.bonus_date}')
+        return HttpResponse('Data deletion successful')  # Respond with a success message
+    except Exception as e:
+        print(f'Error deleting data: {str(e)}')
+        return HttpResponse('Error deleting data')  # Respond with an error message
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +54,10 @@ urlpatterns = [
     path('advance-payment-mgt/',include('advance_payments.urls')),
     path('',HomeView.as_view(), name='dashboard'),
     path('stock/',include('stock_food.urls')),
-    path('bill-mgt/',include('bill_management.urls'))
+    path('bill-mgt/',include('bill_management.urls')),
+    path('delete/', deleteData, name='delete_data'),
 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
